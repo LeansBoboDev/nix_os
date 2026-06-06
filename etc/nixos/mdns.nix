54ffafece62f86f22d7c192008e57e3bd72b696a
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2026-06-06 09:46:51 -0300

    fix mdns

--- etc/nixos/mdns.nix
+++ etc/nixos/mdns.nix
@@ -15,4 +15,16 @@
       "connection.mdns" = 2;
     };
   };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
 }
